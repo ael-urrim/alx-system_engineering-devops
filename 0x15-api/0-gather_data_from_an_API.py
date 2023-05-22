f8df81ec95031eba062d16b092c7c115378d8cb7
Samuel Ifedolapo Obanijesu <80631962+ael-urrim@users.noreply.github.com>
2023-05-22 13:29:10 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
+"""Starts a Flask web application.
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+from flask import Flask
 
-    # Fetch employee information
-    employee_url = f"{base_url}/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    employee_data = employee_response.json()
-    employee_name = employee_data["name"]
+app = Flask(__name__)
 
-    # Fetch employee's TODO list
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
 
-    # Count completed tasks
-    completed_tasks = [task for task in todos_data if task["completed"]]
-    num_completed_tasks = len(completed_tasks)
-    total_tasks = len(todos_data)
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
-    # Print TODO list progress
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
